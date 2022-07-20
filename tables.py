1f55eca27a05f21bde6f30a7fc8a512586ada2b1
Do1e <dpj.email@qq.com>
2022-07-20 21:25:31 +0800

    rm old version

--- tables.py
+++ tables.py
@@ -2,7 +2,7 @@ import numpy as np
 from gen_jpeg_standard_quantization_table import gen_quant_table_by_quality
 
 # 亮度量化表和色度量化表
-std_luminance_quant_tbl, std_chrominance_quant_tbl = gen_quant_table_by_quality(30)
+std_luminance_quant_tbl, std_chrominance_quant_tbl = gen_quant_table_by_quality(100)
 # 亮度直流量范式哈夫曼编码表
 std_huffman_DC0 = np.array(
 	[0, 0, 7, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0,
