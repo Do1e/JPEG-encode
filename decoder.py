c094958dafe0328d9b46246d6c5cd67e8b697294
do1e <dpj.email@qq.com>
2022-07-25 22:07:34 +0800

    begin to debug

--- decoder.py
+++ decoder.py
@@ -1,9 +1,10 @@
+from base64 import decode
 import cv2
 import numpy as np
 import argparse
 
-import tables
 import decoder_utils
+import common_utils
 
 
 parser = argparse.ArgumentParser()
@@ -13,3 +14,80 @@ args = parser.parse_args()
 
 with open(args.input, 'rb') as f:
 	data = f.read()
+data = np.frombuffer(data, np.uint8)
+nowIdx = decoder_utils.decode_head(data)
+if nowIdx is None or data[-2] != 0xff or data[-1] != 0xd9:
+	print("Error: unsupported jpeg format")
+	exit(1)
+data = data[nowIdx:-2]
+
+resimg = np.zeros((decoder_utils.h, decoder_utils.w, decoder_utils.d), np.uint8)
+last_block_dc = [0, 0, 0]
+nowLeft = 0
+nowRight = 0
+nowx = 0
+nowy = 0
+nowIdx, buffer = decoder_utils.load_buffer(data, '', 0)
+
+while nowIdx < data.shape[0]:
+	nowblock_decoded = np.zeros((8, 8, decoder_utils.d), np.int16)
+	for i in range(1, decoder_utils.d+1):
+		nowblock = np.zeros((64), np.int16)
+		nowblockIdx = 0
+		# decode DC
+		while 1:
+			try:
+				var = decoder_utils.DC_color_huffman_dict[i][buffer[nowLeft:nowRight+1]]
+				num = int(buffer[nowRight+1:nowRight+var+1], 2)
+				if buffer[nowRight+1] == '0':
+					num = -((1<<var) - 1) + num
+				nowblock[nowblockIdx] = num
+				nowblockIdx += 1
+				nowLeft = nowRight + var + 1
+				nowRight = nowLeft
+				if nowRight >= len(buffer):
+					nowIdx, buffer = decoder_utils.load_buffer(data, buffer, nowIdx)
+				break
+			except KeyError:
+				nowRight += 1
+				if nowRight >= len(buffer):
+					nowIdx, buffer = decoder_utils.load_buffer(data, buffer, nowIdx)
+		last_block_dc[i-1] = nowblock[0]
+		buffer = buffer[nowRight:]
+		nowLeft = 0
+		nowRight = 0
+		# decode AC
+		while 1:
+			try:
+				var = decoder_utils.AC_color_huffman_dict[i][buffer[nowLeft:nowRight+1]]
+				num0 = var >> 4
+				num1 = var & 0xf
+				nowblockIdx += num0
+				if num0 == 15 and num1 == 0:
+					nowblockIdx += 1
+				elif num0 == 0 and num1 == 0:
+					break
+				else:
+					num = int(buffer[nowRight+1:nowRight+num1+1], 2)
+					if buffer[nowRight+1] == '0':
+						num = -((1<<num1) - 1) + num
+					nowblock[nowblockIdx] = num
+					nowblockIdx += 1
+				nowLeft = nowRight + num1 + 1
+				nowRight = nowLeft
+				if nowRight >= len(buffer):
+					nowIdx, buffer = decoder_utils.load_buffer(data, buffer, nowIdx)
+				if nowblockIdx == 64:
+					break
+				assert nowblockIdx < 64
+			except KeyError:
+				nowRight += 1
+				if nowRight >= len(buffer):
+					nowIdx, buffer = decoder_utils.load_buffer(data, buffer, nowIdx)
+		nowblock_dezz = decoder_utils.zz2block(nowblock)
+		nowblock_dequt = nowblock_dezz * decoder_utils.color_quant_dict[i]
+		nowblock_idct = cv2.idct(nowblock_dequt).round() + 128
+		nowblock_idct[nowblock_idct < 0] = 0
+		nowblock_idct[nowblock_idct > 255] = 255
+		nowblock_decoded[:, :, i-1] = nowblock_idct.astype(np.uint8)
+	resimg[nowx:nowx+8, nowy:nowy+8, :] = cv2.cvtColor(nowblock_idct, cv2.COLOR_YCrCb2BGR)
---
+++ decoder_utils.py
@@ -0,0 +1,132 @@
+import numpy as np
+import common_utils
+
+global h, w, d
+global quant_dict, color_quant_dict
+quant_dict = {}
+color_quant_dict = {}
+global DHTs_DC, DHTs_AC
+DHTs_DC = {}
+DHTs_AC = {}
+global DC_color_huffman_dict, AC_color_huffman_dict
+DC_color_huffman_dict = {}
+AC_color_huffman_dict = {}
+
+def zz2block(block: list) -> np.ndarray:
+	re = np.empty((8, 8), np.int16)
+	# 当前在block的位置
+	pos = np.array([0, 0])
+	# 定义四个扫描方向
+	R = np.array([0, 1])
+	LD = np.array([1, -1])
+	D = np.array([1, 0])
+	RU = np.array([-1, 1])
+	for i in range(0, 64):
+		# re[i] = block[pos[0], pos[1]]
+		re[pos[0], pos[1]] = block[i]
+		if(((pos[0] == 0) or (pos[0] == 7)) and (pos[1] % 2 == 0)):
+			pos = pos + R
+		elif(((pos[1] == 0) or (pos[1] == 7)) and (pos[0] % 2 == 1)):
+			pos = pos + D
+		elif((pos[0] + pos[1]) % 2 == 0):
+			pos = pos + RU
+		else:
+			pos = pos + LD
+	return re
+
+def decode_DHT(data: np.ndarray) -> int:
+	global DHTs_DC, DHTs_AC
+	if data[0] >> 4 == 0:
+		nowtbl = common_utils.DHT2tbl(data[1:])
+		nowdict = {}
+		for item in nowtbl:
+			nowdict[item.str_code] = item.symbol
+		DHTs_DC[data[0] & 0x0f] = nowdict.copy()
+	elif data[0] >> 4 == 1:
+		nowtbl = common_utils.DHT2tbl(data[1:])
+		nowdict = {}
+		for item in nowtbl:
+			nowdict[item.str_code] = item.symbol
+		DHTs_AC[data[0] & 0x0f] = nowdict.copy()
+	else:
+		return None
+	return 0
+
+def decode_head(data: np.ndarray) -> int:
+	nowIdx = 0
+	# SOI
+	if data[nowIdx] != 0xff or data[nowIdx+1] != 0xd8:
+		return None
+	# APP0
+	nowIdx += 2
+	if data[nowIdx] != 0xff or data[nowIdx+1] != 0xe0:
+		return None
+	nowLen = (data[nowIdx+2] << 8) | data[nowIdx+3]
+	nowIdx += 2 + nowLen
+	# DQT_0
+	if data[nowIdx] != 0xff or data[nowIdx+1] != 0xdb:
+		return None
+	nowLen = (data[nowIdx+2] << 8) | data[nowIdx+3]
+	if nowLen != 67:
+		return None
+	if data[nowIdx+4] >> 4 != 0:
+		return None
+	global quant_dict
+	nowtable = zz2block(data[nowIdx+5:nowIdx+69])
+	quant_dict[data[nowIdx+4] & 0x0f] = nowtable.copy()
+	nowIdx += 2 + nowLen
+	# DQT_1
+	if data[nowIdx] != 0xff or data[nowIdx+1] != 0xdb:
+		return None
+	nowLen = (data[nowIdx+2] << 8) | data[nowIdx+3]
+	if nowLen != 67:
+		return None
+	if data[nowIdx+4] >> 4 != 0:
+		return None
+	nowtable = zz2block(data[nowIdx+5:nowIdx+69])
+	quant_dict[data[nowIdx+4] & 0x0f] = nowtable.copy()
+	nowIdx += 2 + nowLen
+	# SOF0
+	if data[nowIdx] != 0xff or data[nowIdx+1] != 0xc0:
+		return None
+	nowLen = (data[nowIdx+2] << 8) | data[nowIdx+3]
+	if data[nowIdx+4] != 8:
+		return None
+	global h, w, d
+	h = (data[nowIdx+5] << 8) | data[nowIdx+6]
+	w = (data[nowIdx+7] << 8) | data[nowIdx+8]
+	d = data[nowIdx+9]
+	if d != 3:
+		return None
+	if data[nowIdx+11] != 17 or data[nowIdx+14] != 17 or data[nowIdx+17] != 17:
+		return None
+	global color_quant_dict
+	color_quant_dict[data[nowIdx+10]] = quant_dict[data[nowIdx+12]]
+	color_quant_dict[data[nowIdx+13]] = quant_dict[data[nowIdx+15]]
+	color_quant_dict[data[nowIdx+16]] = quant_dict[data[nowIdx+18]]
+	nowIdx += 2 + nowLen
+	# DHT
+	while data[nowIdx] == 0xff and data[nowIdx+1] == 0xc4:
+		nowLen = (data[nowIdx+2] << 8) | data[nowIdx+3]
+		status = decode_DHT(data[nowIdx+4:nowIdx+2+nowLen])
+		if status is None:
+			return None
+		nowIdx += 2 + nowLen
+	# SOS
+	if data[nowIdx] != 0xff or data[nowIdx+1] != 0xda:
+		return None
+	nowLen = (data[nowIdx+2] << 8) | data[nowIdx+3]
+	if data[nowIdx+4] != 3:
+		return None
+	global DC_color_huffman_dict
+	for i in range(d):
+		DC_color_huffman_dict[data[nowIdx+5+2*i]] = DHTs_DC[data[nowIdx+6+2*i] >> 4]
+		AC_color_huffman_dict[data[nowIdx+5+2*i]] = DHTs_AC[data[nowIdx+6+2*i] & 0x0f]
+	nowIdx += 2 + nowLen
+	return nowIdx
+
+def load_buffer(data:np.ndarray ,buffer: str, nowIdx: int) -> list:
+	loadLen = min(data.shape[0] - nowIdx, 8)
+	for i in range(nowIdx, nowIdx + loadLen):
+		buffer += bin(data[i])[2:].zfill(8)
+	return nowIdx + loadLen, buffer
